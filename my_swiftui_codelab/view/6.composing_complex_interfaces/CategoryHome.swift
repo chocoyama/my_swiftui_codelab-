813c7b4418a5e380208099dd1ead65d9988f1932
Takuya Yokoyama <puripuripuririnrinririn@gmail.com>
2019-09-15 01:00:25 +0900

    Working with UI Controls

--- my_swiftui_codelab/view/6.composing_complex_interfaces/CategoryHome.swift
+++ my_swiftui_codelab/view/6.composing_complex_interfaces/CategoryHome.swift
@@ -54,7 +54,7 @@ struct CategoryHome: View {
             .navigationBarTitle(Text("Featured"))
             .navigationBarItems(trailing: profileButton)
             .sheet(isPresented: $showingProfile) {
-                Text("User Profile")
+                ProfileHost()
             }
         }
     }
---
+++ my_swiftui_codelab/view/7.working_with_ui_controls/ProfileHost.swift
@@ -0,0 +1,50 @@
+//
+//  ProfileHost.swift
+//  my_swiftui_codelab
+//
+//  Created by Takuya Yokoyama on 2019/09/14.
+//  Copyright © 2019 chocoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var mode
+    @State var profile = Profile.default
+    @State var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                if self.mode?.wrappedValue == .active {
+                    Button(action: {
+                        self.profile = self.draftProfile
+                        self.mode?.animation().wrappedValue = .inactive
+                    }) {
+                        Text("Done")
+                    }
+                }
+                
+                Spacer()
+                
+                EditButton()
+            }
+            
+            if self.mode?.wrappedValue == .inactive {
+                ProfileSummary(profile: self.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onDisappear {
+                        self.draftProfile = self.profile
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
